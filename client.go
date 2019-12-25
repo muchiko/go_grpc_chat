f06ba274a109b4e754c7f2ec2e6215f36bf6ac86
muchiko <shoji0521@gmail.com>
2019-12-25 12:16:16 +0900

    first commit

---
+++ client.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/muchiko/go_grpc_chat/client"
+)
+
+func main() {
+	client.Run()
+}
---
+++ server.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/muchiko/go_grpc_chat/server"
+)
+
+func main() {
+	server.Run()
+}
